5488e3046b6a03318acdaac1967f926d28385fee
Ridan Vandenbergh <RidanVandenbergh@gmail.com>
2018-08-02 19:52:32 +0200

    moved to libgdx

---
+++ desktop/src/me/zeroeightsix/jtcui/desktop/DesktopLauncher.java
@@ -0,0 +1,11 @@
+package me.zeroeightsix.jtcui.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Launcher(), config);
+	}
+}
---
+++ desktop/src/me/zeroeightsix/jtcui/desktop/Launcher.java
@@ -0,0 +1,46 @@
+package me.zeroeightsix.jtcui.desktop;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+public class Launcher extends ApplicationAdapter {
+
+	private Viewport viewport;
+	private OrthographicCamera camera;
+
+	@Override
+	public void create () {
+	    camera = new OrthographicCamera();
+	    camera.setToOrtho(true);
+	    viewport = new ScreenViewport(camera);
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+//		batch.begin();
+//		batch.draw(img, 0, 0);
+//		batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+//		batch.dispose();
+//		img.dispose();
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		super.resize(width, height);
+        viewport.update(width, height);
+        camera.update();
+	}
+}
