4d5dde08bf685077aa869e0906435e1d5c99e885
Ridan Vandenbergh <RidanVandenbergh@gmail.com>
2018-08-05 20:28:51 +0200

    rework grow

--- core/src/me/zeroeightsix/jtcui/layout/Grow.java
+++ core/src/me/zeroeightsix/jtcui/layout/Grow.java
@@ -3,6 +3,22 @@ package me.zeroeightsix.jtcui.layout;
 /**
  * Created by 086 on 23/04/2018.
  */
-public enum Grow {
-    NONE, ALWAYS
+public class Grow {
+
+    public static final Grow NO_GROW = new Grow(-1);
+
+    float modifier;
+
+    public Grow(float modifier) {
+        this.modifier = modifier;
+    }
+
+    public float getModifier() {
+        return modifier;
+    }
+
+    public void setModifier(float modifier) {
+        this.modifier = modifier;
+    }
+
 }
