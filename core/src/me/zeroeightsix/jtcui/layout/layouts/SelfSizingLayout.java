0e59238c95a814e2cf92b54057095658ad188985
Ridan Vandenbergh <RidanVandenbergh@gmail.com>
2018-08-06 15:24:55 +0200

    merge packing and expanding into one layout

---
+++ core/src/me/zeroeightsix/jtcui/layout/layouts/SelfSizingLayout.java
@@ -0,0 +1,71 @@
+package me.zeroeightsix.jtcui.layout.layouts;
+
+import me.zeroeightsix.jtcui.Fat;
+import me.zeroeightsix.jtcui.Space;
+import me.zeroeightsix.jtcui.component.Component;
+import me.zeroeightsix.jtcui.component.Container;
+
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @author 086
+ */
+public abstract class SelfSizingLayout extends AbstractLayout {
+
+    Type type;
+
+    public SelfSizingLayout(Type type) {
+        this.type = type;
+    }
+
+    private void expand(Component component) {
+        Optional.ofNullable(component.getParent()).ifPresent(parent -> {
+            Space space = getSlimSpace(parent);
+            component.getSpace().xProperty().set(space.xProperty().get());
+            component.getSpace().yProperty().set(space.yProperty().get());
+            component.getSpace().widthProperty().set(space.widthProperty().get());
+            component.getSpace().heightProperty().set(space.heightProperty().get());
+        });
+    }
+
+    private void pack(Component component) {
+        Optional.ofNullable(component.getChildren()).ifPresent(children -> {
+            final double[] w = {0};
+            final double[] h = {0};
+            children.forEach(child -> {
+                Space space = child.getSpace();
+                w[0] = Math.max(w[0], space.xProperty().get() + space.widthProperty().get());
+                h[0] = Math.max(h[0], space.xProperty().get() + space.widthProperty().get());
+            });
+            Space space = component.getSpace();
+            int fw = 0, fh = 0;
+            if (component instanceof Container) {
+                Fat fat = ((Container) component).getFat();
+                fw = fat.getRight();
+                fh = fat.getBottom();
+            }
+            space.widthProperty().set(w[0]+fw);
+            space.heightProperty().set(h[0]+fh);
+        });
+    }
+
+    @Override
+    public void update(Component component) {
+        switch (type) {
+            case PACKING:
+                organise(component);
+                pack(component);
+                break;
+            case EXPANDING:
+                expand(component);
+                organise(component);
+                break;
+        }
+    }
+
+    public enum Type {
+        PACKING, EXPANDING
+    }
+
+}
---
+++ core/src/me/zeroeightsix/jtcui/layout/layouts/VerticalLayout.java
@@ -0,0 +1,19 @@
+package me.zeroeightsix.jtcui.layout.layouts;
+
+import me.zeroeightsix.jtcui.component.Component;
+
+/**
+ * @author 086
+ */
+public class VerticalLayout extends SelfSizingLayout {
+
+    public VerticalLayout(Type type) {
+        super(type);
+    }
+
+    @Override
+    public void organise(Component component) {
+
+    }
+
+}
