47b06fb8057ff27af181c491beee4154b1c08811
Ridan Vandenbergh <RidanVandenbergh@gmail.com>
2018-08-26 17:33:01 +0200

    document Component

--- core/src/me/zeroeightsix/jtcui/component/Component.java
+++ core/src/me/zeroeightsix/jtcui/component/Component.java
@@ -1,7 +1,9 @@
 package me.zeroeightsix.jtcui.component;
 
-import me.zeroeightsix.jtcui.Space;
+import com.sun.istack.internal.NotNull;
+import com.sun.istack.internal.Nullable;
 import me.zeroeightsix.jtcui.Requirements;
+import me.zeroeightsix.jtcui.Space;
 import me.zeroeightsix.jtcui.handle.ComponentHandle;
 import me.zeroeightsix.jtcui.handle.MouseHandler;
 import me.zeroeightsix.jtcui.layout.layouts.Layout;
@@ -13,18 +15,70 @@ import java.util.List;
  */
 public interface Component {
 
+    /**
+     * @return The dimensions and location of this component
+     * @see Space
+     */
     Space getSpace();
+
+    /**
+     * @return The requirements for this component
+     */
     Requirements getRequirements();
 
+    /**
+     * @return This components parent, if any.
+     */
+    @Nullable
     Component getParent();
+
+    /**
+     * @return A list of this components children, if any.
+     */
+    @Nullable
     List<Component> getChildren();
+
+    /**
+     * @return This components layout
+     */
+    @Nullable
     Layout getLayout();
+
+    /**
+     * Get the handle for this component. If null, JTC will use previously installed handles.
+     *
+     * @return The handle for this component
+     */
+    @Nullable
     ComponentHandle getHandle();
+
+    /**
+     * @return A list of this components mouse handlers
+     */
+    @NotNull
     List<MouseHandler> getMouseHandlers();
 
-    void setParent(Component component);
-    void setLayout(Layout layout);
+    /**
+     * Sets a new parent for this component. Be careful as this method is not supposed to modify children lists - it should only be used when modifying containers.
+     *
+     * @param component The new parent of this component
+     * @see SimpleContainer
+     */
+    void setParent(@Nullable Component component);
 
+    /**
+     * @param layout The new layout for this component
+     */
+    void setLayout(@Nullable Layout layout);
+
+    /**
+     * Find the component at position (x, y) with origin being this components origin.
+     *
+     * @param x The x-coordinate to search at
+     * @param y The y-coordinate to search at
+     * @return The component found
+     * @see ExplorableContainer
+     */
     Component explore(double x, double y);
 
 }
